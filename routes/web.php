b09bd359ee1de34348790e46b4b83e515ed0bafb
root <novakadam18042000@gmail.com>
2022-02-17 21:47:33 +0100

    First commit

---
+++ routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+/*
+| Base URL redirects to the home page
+*/
+Route::get('/', function () {
+    return redirect('/home');
+});
+
+/*
+| Home page
+*/
+Route::get('/domov', function () {
+    return view('home');
+});
+
+/*
+| Intro page
+*/
+Route::get('/uvod', function () {
+    return view('intro');
+});
+
+/*
+| Map page
+*/
+Route::get('/mapa', function () {
+    return view('map');
+});
