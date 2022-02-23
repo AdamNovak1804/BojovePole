649a2c3bd8433f786f8fff9f2af4c427b312c93c
root <novakadam18042000@gmail.com>
2022-02-23 18:58:42 +0100

    Bootstrap is now working

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,12 +3,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Domov</title>
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css">
 </head>
 <body>
-    <header>
-        <p>Hello world</p>
-    </header>
-    <h1>Home</h1>
+<nav class="navbar navbar-light bg-light">
+  <a href="#" class="navbar-brand mb-0 h1">
+    Hello
+  </a>
+</nav>
 </body>
 </html>
